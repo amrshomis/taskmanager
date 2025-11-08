4ecc48772c85000780ba571b54ca9983bde05e41
amr shomis <amrshomis123@gmail.com>
2025-11-08 22:26:52 +0300

    add list task and edit task and add task to Folder templates/tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    query = request.GET.get('q', '')
+    tasks = Task.objects.filter(title__icontains=query)
+    return render(request, 'tasks/task_list.html', {'tasks': tasks, 'query': query})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+    return render(request, 'tasks/add_task.html')
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        task.title = request.POST['title']
+        task.description = request.POST['description']
+        task.completed = 'completed' in request.POST
+        task.save()
+        return redirect('task_list')
+    return render(request, 'tasks/edit_task.html', {'task': task})
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
